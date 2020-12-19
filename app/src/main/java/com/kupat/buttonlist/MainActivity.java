17117759273b4d7b7671ec5a54fe00a78009b12f
Isman Junian <monopecez@gmail.com>
2020-12-19 13:42:44 +0700

    Programmatically create android button with onClickListener

--- app/src/main/java/com/kupat/buttonlist/MainActivity.java
+++ app/src/main/java/com/kupat/buttonlist/MainActivity.java
@@ -4,32 +4,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    RelativeLayout relativeLayout;
+    LinearLayout mainLayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        relativeLayout = findViewById(R.id.relativeLayout);
-        Button button = new Button(this);
-        button.setText("Dynamically created Button");
-        button.setLayoutParams(new
-                RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button is created dynamically",
-                        Toast.LENGTH_SHORT).show();
+        mainLayer = (LinearLayout) findViewById(R.id.mainLayer);
+
+
+        for (int i = 0; i < 20; i++) {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            LinearLayout layout = new LinearLayout(getApplicationContext());
+            LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            layout.setOrientation(LinearLayout.VERTICAL);
+            layout.setLayoutParams(params);
+            Button button1 = new Button(getApplicationContext());
+            button1.setLayoutParams(params1);
+            button1.setText("button" + (i+1));
+            layout.addView(button1);
+
+            final int idx = i;
+
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println("Clicked button" + (idx+1));
+                }
+            });
+            mainLayer.addView(layout);
             }
-        });
-        if (relativeLayout != null){
-            relativeLayout.addView(button);
         }
     }
-}
