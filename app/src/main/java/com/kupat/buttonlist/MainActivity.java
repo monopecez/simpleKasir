ae6f947108e78dad7d7dc34d32f8e592d81fa5be
Isman Junian <monopecez@gmail.com>
2020-12-19 16:08:07 +0700

    Parse JSON and generate button

--- app/src/main/java/com/kupat/buttonlist/MainActivity.java
+++ app/src/main/java/com/kupat/buttonlist/MainActivity.java
@@ -11,6 +11,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     LinearLayout mainLayer;
     @Override
@@ -19,8 +27,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainLayer = (LinearLayout) findViewById(R.id.mainLayer);
 
+//        String text = "{\"data\":[[\"nama\",[100,200,300]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]]]}";
+        String text = "{\"data\":[[\"nama\",[100,200,300]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]],[\"saya\",[400,500,600]],[\"isman\",[700,800,900]]]}";
+        JSONArray data = getData(text);
+        int nButton = data.length();
 
-        for (int i = 0; i < 20; i++) {
+        final ArrayList<String> list = new ArrayList<String>();
+        try {
+            for (int i=0; i< nButton; i++){
+                list.add(data.getString(i));
+            }
+        } catch (JSONException e) {}
+
+        System.out.println("DEBUG" + list);
+
+        HashMap<String, JSONArray> harga = new HashMap<String, JSONArray>();
+        HashMap<String, Integer> subTotal = new HashMap<String, Integer>();
+        HashMap<String, Integer> qty = new HashMap<String, Integer>();
+
+        String menuTitle;
+        JSONArray hargaArray;
+
+        for (int i = 0; i < nButton; i++) {
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
 
             LinearLayout layout = new LinearLayout(getApplicationContext());
@@ -30,7 +58,20 @@ public class MainActivity extends AppCompatActivity {
             layout.setLayoutParams(params);
             Button button1 = new Button(getApplicationContext());
             button1.setLayoutParams(params1);
-            button1.setText("button" + (i+1));
+
+            try {
+                JSONArray innerData = data.getJSONArray(i);
+                menuTitle = innerData.getString(0);
+                hargaArray = innerData.getJSONArray(1);
+                button1.setText(menuTitle);
+                harga.put(menuTitle, hargaArray);
+                qty.put(menuTitle, 0);
+                subTotal.put(menuTitle, 0);
+
+            } catch (JSONException e){
+                System.out.println(e);
+                button1.setText("NOT FOUND");
+            }
             layout.addView(button1);
 
             final int idx = i;
@@ -38,10 +79,31 @@ public class MainActivity extends AppCompatActivity {
             button1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    System.out.println("Clicked button" + (idx+1));
+                    System.out.println("DEBUG" + list.get(idx));
                 }
             });
             mainLayer.addView(layout);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        public JSONArray getData(String text){
+            String jsonString = text;
+            try {
+                JSONObject jsO = new JSONObject(jsonString);
+                JSONArray res = (JSONArray) jsO.get("data");
+                return res;
+            } catch (JSONException e){
+                System.out.println (e);
+                return new JSONArray();
             }
         }
     }
